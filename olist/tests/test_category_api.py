0e155f3c421a434abdc44dadacb7312fc4a5a3f9
matheus <matheus@tradenamao.com.br>
2021-02-21 14:04:43 -0300

    category api test done

--- olist/tests/test_category_api.py
+++ olist/tests/test_category_api.py
@@ -9,23 +9,22 @@ class AthleteAPITest(APITestCase):
             'category_name': 'Test category',
             'category_description': 'This is a test category',
         }
+        cls.edit_category = {
+            'id': 1,
+            'category_name': 'Test category',
+            'category_description': 'This is a test category',
+        }
 
     def test_get_category_api(self):
-        response = self.client.get('/api/events/')
+        response = self.client.get('/api/categories/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_post_category_api(self):
-        response = self.client.post('/api/events/', self.create_events)
+        response = self.client.post('/api/categories/', self.create_category)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_put_category_api(self):
-        response = self.client.post('/api/events/', self.create_events)
-        id = response.json()['id']
-        response = self.client.put(f'/api/events/{id}/', self.edit_event)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
     def test_patch_category_api(self):
-        response = self.client.post('/api/events/', self.create_events)
+        response = self.client.post('/api/categories/', self.create_category)
         id = response.json()['id']
-        response = self.client.patch(f'/api/events/{id}/', self.patch_event)
+        response = self.client.patch(f'/api/categories/{id}/', self.edit_category)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
